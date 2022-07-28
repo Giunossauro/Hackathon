b49e5da1b7a6821f3d14cc598813a6972b818063
dot_ <muriloholiveira@hotmail.com>
2022-07-27 22:37:59 -0300

    :sparkles: Finishing Students Routes

--- server/src/apps/api/routes/StudentRoute.ts
+++ server/src/apps/api/routes/StudentRoute.ts
@@ -7,5 +7,7 @@ const _controller = new StudentController();
 router.get("/alunos", _controller.getStudents);
 router.get("/alunos/:id", _controller.getStudentById);
 router.post("/alunos", _controller.addStudents);
+router.put("/alunos/:id", _controller.updateStudent);
+router.delete("/alunos/:id", _controller.removeStudent);
 
 export = router;
