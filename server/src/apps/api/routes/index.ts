327684473313f3c53cfae60c869b78e755b32a99
Giuliano Morelli <giuandroide@gmail.com>
2022-07-28 05:36:07 -0300

    refactor e add rotas e metodos para cursos

--- server/src/apps/api/routes/index.ts
+++ server/src/apps/api/routes/index.ts
@@ -1,4 +1,5 @@
 import studentRouter from "./StudentRoute";
 import teacherRouter from "./TeacherRoute";
+import courseRouter from "./CourseRoute";
 
-export default [studentRouter, teacherRouter];
+export default [studentRouter, teacherRouter, courseRouter];
