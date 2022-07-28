e0d01270ae2e5a5839323f7941ab79ae43b1550e
dot_ <muriloholiveira@hotmail.com>
2022-07-27 21:08:09 -0300

    :sparkles: Improving Teachers Route

--- server/src/apps/api/controllers/TeacherController.ts
+++ server/src/apps/api/controllers/TeacherController.ts
@@ -26,26 +26,39 @@ export class TeacherController {
 	getTeacherById = async (req: Request<TeacherRequest>, res: Response, next: NextFunction) => {
 		const { id } = req.params;
 
-		//valida se é número
+		if (!id || !Number(id)) {
+			return res.status(400).json({ result: "ERRO: ID inválido." });
+		}
 
-		return res.status(200).json({
-			result: await this.#service.getTeacherById(Number(id))
+		const result = await this.#service.getTeacherById(Number(id));
+
+		return res.status(result.status).json({
+			result: result.msg
 		})
 	}
 
 	addTeachers = async (req: Request<TeacherRequest>, res: Response, next: NextFunction) => {
 		const { nome, cpf, email, senha, excluido = false } = req.body;
 
-		//valida tipos dos campos
+		if (nome == "" || email == "" || senha == "") {
+			return res.status(400).json({ result: "ERRO: Preencha todos os campos." });
+		}
 
-		return res.status(200).json({
-			result: await this.#service.addTeacher(
-				String(nome),
-				String(cpf),
-				String(email),
-				String(senha),
-				Boolean(excluido)
-			)
+		if (nome.length > 60 || email.length > 60 || senha.length > 500) {
+			return res.status(400).json({ result: "ERRO: Número máximo de caracteres excedido." });
+		}
+
+		const emailPattern = email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g);
+
+		if (!emailPattern) {
+			return res.status(400).json({ result: "ERRO: Padrão de email inválido." });
+		}
+
+		const result = await this.#service.addTeacher(String(nome), String(cpf), String(email), String(senha), Boolean(excluido));
+
+
+		return res.status(result.status).json({
+			result: result.msg
 		})
 	};
 }
--- server/src/services/TeacherServices.ts
+++ server/src/services/TeacherServices.ts
@@ -10,10 +10,17 @@ export class TeacherService {
 
 	getTeacherById = async (id: Number) => {
 		const teacherById = await db.default.select("*").from('professores').where({ id: id });
-		return teacherById;
+
+		return { status: 200, msg: teacherById };
 	}
 
 	addTeacher = async (nome: String, cpf: String, email: String, senha: String, excluido: Boolean) => {
+		const findTeacher = await db.default.select("*").from('professores').where({ email: email });
+
+		if (findTeacher.length > 0) {
+			return { status: 400, msg: "ERRO: Falha ao cadastrar usuário. Verifique email e senha." }
+		}
+
 		const newUser = {
 			nome: nome,
 			cpf: cpf,
@@ -24,6 +31,10 @@ export class TeacherService {
 
 		await db.default("professores").insert(newUser)
 
-		return newUser;
+		return { status: 201, msg: newUser }
 	}
+
+	updateTeacher = async (id: Number) => { }
+
+	removeTeacher = async (id: Number) => { }
 }
